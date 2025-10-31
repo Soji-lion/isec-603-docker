ed9be3f20eac208a98d028b26ea8078643b2e6f3
Soji Lion <67437594+Soji-lion@users.noreply.github.com>
2025-10-31 01:46:55 -0600

    Enhanced  scripts' automation

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-#sudo docker load -i ./images/internal_host.tar
-#sudo docker load -i ./images/juice_shop.tar
-#sudo docker load -i ./images/kali.tar
-#sudo docker load -i ./images/router.tar
+xhost +local:docker
 sudo docker compose up
